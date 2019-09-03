e16825cbd2b357057998013458d3222857dd29b2
BluRe-CN <blurecn@gmail.com>
2019-09-03 16:07:13 +0100

    Added bot date class

---
+++ BotDate.js
@@ -0,0 +1,29 @@
+class BotDate {
+
+    getDateAndTime(which) {
+
+        var currentDate = new Date();
+        var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        var date = currentDate.getDate();
+        var month = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        var year = currentDate.getFullYear();
+        var hour = currentDate.getHours();
+        var min = currentDate.getMinutes();
+        var def = "th ";
+        switch (which) {
+            case "date":
+                if (date === 3 || date === 23) {
+                    def = "rd ";
+                } else if (date === 1 || date === 21) {
+                    def = "st ";
+                } else if (date === 2 || date === 22) {
+                    def = "nd ";
+                }
+                return day[currentDate.getDay()] + ", " + date + def + month[currentDate.getMonth()] + ", " + year;
+            case "time":
+                return hour + ":" + min;
+        }
+    }
+
+}
+module.exports = BotDate;
